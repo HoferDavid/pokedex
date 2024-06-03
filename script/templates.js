d187022a230c6ba22d6e863308cd217ef0e99797
David Hofer <chassis.poppets0x@icloud.com>
2024-06-03 10:01:36 +0200

    added weight and height calc

--- script/templates.js
+++ script/templates.js
@@ -1,5 +1,6 @@
 function generateRenderPokemonsHTML(pokemon, index, typesHTML) {
-  const pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const pokemonName =
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
   let bgColor = pokemon.details.types[0].type["name"];
 
   return /*html*/ `
@@ -17,14 +18,26 @@ function generateRenderPokemonsHTML(pokemon, index, typesHTML) {
 }
 
 function generateOpenOverlayHTML(pokemon, index) {
-  const pokemonName = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
+  const pokemonName =
+    pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
   let bgColor = pokemon.details.types[0].type["name"];
+  let weight = (pokemon.details.weight / 10) * 2.20462;
+  let roundedWeight = weight.toFixed(1);
+  let heightInFeet = (pokemon.details.height / 10) * 3.28084;
+  const feet = Math.floor(heightInFeet);
+  const inches = Math.round((heightInFeet - feet) * 12);
 
   return /*html*/ `
     <div class="overlayPokemonCardContainer ${bgColor}">
-        <div class="${pokemon.details.types[0].type["name"]}">${pokemonName}</div>
+        <div class="${
+          pokemon.details.types[0].type["name"]
+        }">${pokemonName}</div>
+        <div>HP ${pokemon.details.stats[0].base_stat}</div>
         <div class="overlayImgBox">
-            <img src="${pokemon.details.sprites.other["official-artwork"].front_default}">
+            <img src="${
+              pokemon.details.sprites.other["official-artwork"].front_default
+            }">
+            <div id="imgStats">NO. ${pokemon.details.id} HT: ${feet}'${inches < 10 ? "0" : ""}${inches}" WT: ${roundedWeight} lbs.</div>
         </div>
 
     </div>
